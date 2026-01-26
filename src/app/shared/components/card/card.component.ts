019a8e3514cc43f326468cd5ca92bdea6d8811db
Paulo Gramacho <paulo.gramacho@mundpay.com>
2026-01-25 21:55:36 -0300

    construcao header-component; ajuste layout no card-component;

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,15 +1,22 @@
-import { Component, input } from '@angular/core';
+import { Component, input, computed } from '@angular/core';
 import { CurrencyPipe, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [NgClass, CurrencyPipe],
+  imports: [CurrencyPipe],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
 export class CardComponent {
 
   currency = input({title: '', currentValue: 0, variation: '', updated: ''});
+
+  valueColorClass = computed(() => {
+    const value = this.currency().currentValue;
+    if (value <= 1) return 'text-red';
+    if (value > 5) return 'text-blue';
+    return 'text-green';
+  });
 
 }
